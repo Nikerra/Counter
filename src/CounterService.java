763976e3e6d33d468f007068ca0b0386574b09ba
Nikerra <nikerra90@gmail.com>
2023-03-26 14:38:08 +0300

    version 0.2.4.1

--- src/CounterService.java
+++ src/CounterService.java
@@ -7,11 +7,11 @@ import java.util.stream.Stream;
 public class CounterService {
 
 
-    private final static String fileName = "serial";
+    private final static String FILE_NAME = "serial";
 
 //Загрузка состояния
      private Integer load() {
-        try(ObjectInputStream objIstr = new ObjectInputStream(new FileInputStream(fileName))) {
+        try(ObjectInputStream objIstr = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
             return objIstr.readInt();
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -20,7 +20,7 @@ public class CounterService {
 
 //  Сохранение состояния
     protected void save(int saveCounter) {
-        try (ObjectOutputStream objOStr = new ObjectOutputStream(new FileOutputStream(fileName))) {
+        try (ObjectOutputStream objOStr = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
             objOStr.writeInt(saveCounter);
         } catch (IOException e) {
             e.printStackTrace();
@@ -33,7 +33,7 @@ public class CounterService {
     protected int fileExists() {
 //      Для работы счетчика через файл проверяем наличие файла куда будем писать его данные и если
 //      не нашли то создаем файл(так же для первого запуска)
-        return Optional.of(new File(fileName))
+        return Optional.of(new File(FILE_NAME))
                 .filter(File::exists)
                 .map(f->load())
                 .orElse(0);
